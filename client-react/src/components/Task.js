379a7af835da52e1aefd26eed15682cde9eae6ab
Karelle Hofler <karellehofler@gmail.com>
2020-04-05 23:31:53 -0400

    Task.js CRUD updated

--- client-react/src/components/Task.js
+++ client-react/src/components/Task.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import '../task.min.css'
+
 class Task extends React.Component {
   constructor(props) {
     super(props);
@@ -14,18 +15,18 @@ class Task extends React.Component {
 
   getData = () => {
     // Java Spring Boot uses port 8080
-    //let url = "http://localhost:8080/tasks";
+    let url = "http://localhost:8080/tasks";
 
     // C# dotnetcore uses port 5000
     //let url = "http://localhost:5000/projects";
 
     // Express uses port 3001 (react uses 3000)
-    let url = "http://localhost:3001/tasks";
+    // let url = "http://localhost:3001/tasks";
     axios.get(url).then(response => this.setState({ tasks: response.data }));
   };
 
   addTask = () => {
-    let url = "http://localhost:3001/tasks";
+    let url = "http://localhost:8080/tasks";
     axios.post(url, { name: this.taskName.current.value }).then(response => {
       // refresh the data
       this.getData();
@@ -35,13 +36,13 @@ class Task extends React.Component {
   };
 
   updateTask = (id) => {
-    let url = "http://localhost:3001/tasks/" + id + "/complete";
+    let url = "http://localhost:8080/tasks/" + id + "/complete";
     axios.put(url, { taskid: this.id, isComplete: this.isComplete }).then(response => {
       this.getData();
     });
   };
   deleteTask = (id) => {
-    let url = `http://localhost:3001/tasks/${id}/delete`;
+    let url = `http://localhost:8080/tasks/${id}/delete`;
     axios.delete(url, { taskid: this.id}).then(response => {
       console.log(response)
     });
