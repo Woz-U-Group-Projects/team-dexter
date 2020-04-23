751dd4e4b9c5051fbfc5294e2f27adbb151090e9
witneyH1983 <witney.hardin@outlook.com>
2020-04-22 22:59:16 -0500

    adding app.js

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import  Task from "./components/Task";
 import "./App.css";
+import Search from "./components/search";
 
 function App() {
   return (
     <div className="App">
-      <Task />
+      <Search />
     </div>
   );
 }
