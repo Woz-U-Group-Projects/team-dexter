41f566ba99c05ff4c5884ba5ef6d7da35953ee7d
witneyH1983 <witney.hardin@outlook.com>
2020-04-22 22:57:46 -0500

    adding search.js search.css app.js

--- client-react/src/components/search.js
+++ client-react/src/components/search.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import '../search.css';
+import './search.css';
 class search extends React.Component {
   constructor(props) {
     super(props);
