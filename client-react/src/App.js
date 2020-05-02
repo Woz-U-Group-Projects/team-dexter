bc56a9cb1c0250a606e90e02044e65d0e9406ebd
kreative94 <karellehofler@gmail.com>
2020-05-02 10:53:10 -0400

    Updated App

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Routes from "./components/Routes";
 
-<<<<<<< HEAD
 class App extends React.Component {
 
   render() {
@@ -10,14 +9,6 @@ class App extends React.Component {
       <Routes />
     );
   }
-=======
-function App() {
-  return (
-    <div className="App">
-      <Routes/>
-    </div>
-  );
->>>>>>> 21048a7678ce4d9aaa2c5e06c5456b529f496919
 }
 
 
